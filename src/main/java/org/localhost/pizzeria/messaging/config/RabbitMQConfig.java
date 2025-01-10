cce4a1c28d510d399ab99227c053a897e83869d1
marski84 <marski705@gmail.com>
2025-01-10 18:19:00 +0100

    feat (messaging, amq broker): implemented dead letter queue with retrying

--- src/main/java/org/localhost/pizzeria/messaging/config/RabbitMQConfig.java
+++ src/main/java/org/localhost/pizzeria/messaging/config/RabbitMQConfig.java
@@ -1,11 +1,6 @@
 package org.localhost.pizzeria.messaging.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,13 +16,14 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.exchange.supply-check}")
     private String exchangeName;
 
+    @Value("${rabbitmq.exchange.supply-check-dlq-exchange}")
+    private String dlqExchangeName;
+
     @Value("${rabbitmq.routing-key.supply-check}")
     private String routingKey;
 
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, true);
-    }
+    @Value("${rabbitmq.queue.supply-check-dlq}")
+    private String queueDlq;
 
     @Bean
     TopicExchange exchange() {
@@ -35,11 +31,36 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
+    FanoutExchange deadLetterExchange() {
+        return new FanoutExchange(dlqExchangeName);
+    }
+
+    @Bean
+    Queue deadLetterQueue() {
+        return QueueBuilder.durable(queueDlq)
+                .build();
+    }
+
+    @Bean
+    Queue queue() {
+        return QueueBuilder.durable(queueName)
+                .withArgument("x-dead-letter-exchange", dlqExchangeName)
+                .build();
+    }
+
+    @Bean
+    Binding binding() {
         return BindingBuilder
-                .bind(queue)
-                .to(exchange)
+                .bind(queue())
+                .to(exchange())
                 .with(routingKey);
+    }
+
+    @Bean
+    Binding deadLetterBinding() {
+        return BindingBuilder
+                .bind(deadLetterQueue())
+                .to(deadLetterExchange());
     }
 
     @Bean
@@ -47,4 +68,14 @@ public class RabbitMQConfig {
         return new Jackson2JsonMessageConverter();
     }
 
+//    @Bean
+//    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+//            ConnectionFactory connectionFactory,
+//            MessageConverter messageConverter) {
+//        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+//        factory.setConnectionFactory(connectionFactory);
+//        factory.setMessageConverter(messageConverter);
+//        factory.setDefaultRequeueRejected(false);
+//        return factory;
+//    }
 }
--- src/main/java/org/localhost/pizzeria/supplies/system/config/SchedulerConfig.java
+++
@@ -1,4 +0,0 @@
-package org.localhost.pizzeria.supplies.system.config;
-
-public class SchedulerConfig {
-}
